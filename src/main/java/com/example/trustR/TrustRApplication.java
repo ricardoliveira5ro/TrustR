da346e8fc5cba3c3ef2e325f888d2fb0c8014d96
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-12-29 16:08:25 +0000

    init repo and health check endpoint

---
+++ src/main/java/com/example/trustR/TrustRApplication.java
@@ -0,0 +1,13 @@
+package com.example.trustR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trustR/TrustRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trustR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
