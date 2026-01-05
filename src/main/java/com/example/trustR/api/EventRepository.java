ba9b65dd2223e2b0497bfcc23a223382a89f31a3
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-01-05 10:57:30 +0000

    Logging

--- src/main/java/com/example/trustR/api/EventRepository.java
+++ src/main/java/com/example/trustR/api/EventRepository.java
@@ -3,11 +3,15 @@ package com.example.trustR.api;
 import com.example.trustR.event.Event;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class EventRepository {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(EventRepository.class);
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -15,5 +19,7 @@ public class EventRepository {
     @Transactional
     public void saveEvent(Event event) {
         entityManager.persist(event);
+
+        LOGGER.info("Event {} processed", event.getEventId());
     }
 }
