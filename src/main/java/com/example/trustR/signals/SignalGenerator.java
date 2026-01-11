06a1095b4fb2fb52794558564e564817413ed2d6
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-01-11 12:27:00 +0000

    instant signals

--- src/main/java/com/example/trustR/signals/SignalGenerator.java
+++ src/main/java/com/example/trustR/signals/SignalGenerator.java
@@ -1,14 +1,28 @@
 package com.example.trustR.signals;
 
+import com.example.trustR.model.Event;
 import com.example.trustR.model.Signal;
+import com.example.trustR.model.SignalType;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class SignalGenerator {
 
-    public List<Signal> generateSignals() {
-        return null;
+    public List<Signal> generateSignals(Event currentEvent) {
+        List<Signal> signals = new ArrayList<>();
+
+        switch (currentEvent.getEventType()) {
+            case ACTION_FAILED -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.FAILED_ACTION_ATTEMPT, true));
+            case ACTION_SUCCEEDED -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.SUCCESSFUL_ACTION, true));
+            case RATE_LIMIT_EXCEEDED -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.RATE_LIMIT_HIT, true));
+            case POLICY_VIOLATION -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.POLICY_VIOLATION_REPORTED, true));
+            case MANUAL_REVIEW -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.MANUAL_REVIEW_TRIGGERED, true));
+            case ACCOUNT_VERIFIED -> signals.add(Signal.booleanSignal(currentEvent.getActorId(), SignalType.ACCOUNT_VERIFIED_STATUS, true));
+        }
+
+        return signals;
     }
 }
--- src/main/java/com/example/trustR/signals/SignalService.java
+++ src/main/java/com/example/trustR/signals/SignalService.java
@@ -1,10 +1,13 @@
 package com.example.trustR.signals;
 
 import com.example.trustR.model.Event;
+import com.example.trustR.model.Signal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SignalService {
@@ -15,7 +18,7 @@ public class SignalService {
     private SignalRepository signalRepository;
 
     @Autowired
-    public void setSignalEngine(SignalGenerator signalGenerator) {
+    public void setSignalGenerator(SignalGenerator signalGenerator) {
         this.signalGenerator = signalGenerator;
     }
 
@@ -28,5 +31,7 @@ public class SignalService {
         LOGGER.info("Generating Signals");
 
         // SignalEngine to generate signals based on event
+        List<Signal> signals = signalGenerator.generateSignals(currentEvent);
+        signals.forEach(s -> signalRepository.saveSignal(s));
     }
 }
